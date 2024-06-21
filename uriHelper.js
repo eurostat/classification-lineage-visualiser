511e077ffd5731f64c092ba223260cc52fdf5962
hirmore <reigo.hirmo@ext.ec.europa.eu>
2024-06-21 12:06:25 +0200

    Add uriHelper.js with functions to create and modify URLs

---
+++ uriHelper.js
@@ -0,0 +1,27 @@
+export function createConceptURL(baseUri, category) {
+	const urlParts = new URL(baseUri);
+	const { pathname } = urlParts;
+	const path = pathname.split('/');
+
+	const currentVersionYear = path[path.length - 1];
+
+	const yearMatch = currentVersionYear.match(/(\d{4})/);
+	if (!yearMatch) {
+		throw new Error("Invalid year format in URL");
+	}
+
+	const extractedYear = yearMatch[1];
+	const nextVersionYear = (+extractedYear) + 1;
+
+	const upperCaseCategory = category.toUpperCase();
+	path[path.length - 1] = `${upperCaseCategory}${extractedYear}_${upperCaseCategory}${nextVersionYear}`;
+
+	urlParts.pathname = path.join('/');
+
+	return urlParts.toString();
+}
+
+export function modifyYearInURL(baseUri, category, increment = true) {
+  const regex = new RegExp(`(${category})(\\d{4})(?=\\D|$)`, 'gi');
+  return baseUri.replace(regex, (_, p1, p2) => `${p1}${parseInt(p2) + (increment ? 1 : -1)}`);
+}
