c36a9e7be5f35f0db7e18e03b3a40cdd7b8c8f65
hirmore <reigo.hirmo@ext.ec.europa.eu>
2024-06-21 06:41:31 +0200

    Prepare for the main data query

--- select2.js
+++ select2.js
@@ -1,4 +1,5 @@
 import { getDataAndLoadSelect2 } from "./getDataAndLoadSelect2.js";
+import { mainData } from './sampleMainData.js';
 
 $("#categories")
 	.select2({
@@ -24,9 +25,10 @@ $("#categories")
 $("#versions")
 	.select2()
 	.on("select2:select", function (e) {
-		const path = e.params.data.id;
+		const version = e.params.data.id;
 		const category = $("#categories").val();
-		getDataAndLoadSelect2(e.target.id, category, path);
+		const uri = e.params.data.data.uri;
+		getDataAndLoadSelect2(e.target.id, category, uri, version);
 	}).on("select2:clear", function (e) {
 		$("#concepts").empty().prop("disabled", true);
 	});
@@ -34,6 +36,52 @@ $("#versions")
 $("#concepts")
 	.select2()
 	.on("select2:select", function (e) {
-		const path = e.params.data.id;
+		const id = e.params.data.id;
+		const version = $("#versions").val();
 		const category = $("#categories").val();
+		const uri = $("#versions").select2("data")[0].data.uri;
+		queryBuilderForTargetMatch(id, version, category, uri);
 	});
+
+function createModifiedUrl(baseUrl, category) {
+	const urlParts = new URL(baseUrl);
+	const { pathname } = urlParts;
+	const path = pathname.split('/');
+
+	const currentVersionYear = path[path.length - 1];
+
+	const yearMatch = currentVersionYear.match(/(\d{4})/);
+	if (!yearMatch) {
+		throw new Error("Invalid year format in URL");
+	}
+
+	const extractedYear = yearMatch[1];
+	const nextVersionYear = (+extractedYear) + 1;
+
+	const upperCaseCategory = category.toUpperCase();
+	path[path.length - 1] = `${upperCaseCategory}${extractedYear}_${upperCaseCategory}${nextVersionYear}`;
+
+	urlParts.pathname = path.join('/');
+
+	return urlParts.toString();
+}
+
+function incrementYearInURL(url,category) {
+  return url.replace(/(\d{4})(?=\D|$)/g, (match) => parseInt(match) + 1);
+}
+
+function decrementYearInURL(url, category) {
+  const regex = new RegExp(`(${category})(\\d{4})(?=\\D|$)`, 'gi');
+  return url.replace(regex, (match, p1, p2) => `${p1}${parseInt(p2) - 1}`);
+}
+
+	function queryBuilderForTargetMatch(id, version, category, uri) {
+		try {
+			const modifiedUrl = createModifiedUrl(uri, category, version) + '_' + id;
+			console.log(modifiedUrl); // Output: http://data.europa.eu/qw1/prodcom2022/PRODCOM2022_PRODCOM2023
+			console.log(incrementYearInURL(modifiedUrl,category)); // Output: http://data.europa.eu/qw1/prodcom2022/PRODCOM2022_PRODCOM2023
+			console.log(decrementYearInURL(modifiedUrl,category)); // Output: http://data.europa.eu/qw1/prodcom2022/PRODCOM2022_PRODCOM2023
+		} catch (error) {
+			console.error("Error:", error.message);
+		}
+	}
