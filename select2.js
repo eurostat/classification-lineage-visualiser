d49226fece82b2695809d641529a033d0e86e109
hirmore <reigo.hirmo@ext.ec.europa.eu>
2024-07-01 14:31:14 +0200

    Add call to composeGraphData with updated parameters (new test)

--- select2.js
+++ select2.js
@@ -57,6 +57,7 @@ $("#concepts").select2()
 		// console.log("Button clicked:", callerId, category, baseUri, baseYear, conceptId);
 		// composeGraphData(callerId, category, baseUri, baseYear, conceptId);
 		composeGraphData("concepts", "cn", "http://data.europa.eu/xsp/cn2022/CN2022_CN2023", 2022, "846229100080");
+		// composeGraphData("concepts", "cn", "http://data.europa.eu/xsp/cn2022/CN2022_CN2023", 2021, "846299200080");
 
 
 	});
