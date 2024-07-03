f5de103b072290454f2d88846db9d63951223f7f
hirmore <reigo.hirmo@ext.ec.europa.eu>
2024-07-03 11:57:18 +0200

    Remove unused fakeAjaxRequest function

--- ajaxHelper.js
+++ ajaxHelper.js
@@ -39,16 +39,6 @@ export async function makeAjaxRequest(url, method, headers, data, onSuccess, onE
     });
   }
 }
-// Instead of making an AJAX request, read a local JSON file
-export function fakeAjaxRequest(url, method, headers, data, onSuccess, onError, callerId) {
-  console.warn('fakeAjaxRequest called with callerId:', callerId);
-  const path = { versions: 'data/cn2019.json', categories: 'data/cn-versions.json', concepts:'data/CN2021_CN2022_846229100080.json' }[callerId];
-  $.getJSON(path, function(data) {
-    onSuccess(data);
-  }).fail(function(jqxhr, textStatus, error) {
-    onError(jqxhr, textStatus, error);
-  });
-}
 export class RequestQueue {
   constructor(maxConcurrent) {
     this.queue = [];
