368b6e98451dff16acc27a421c761dbe1bac7681
hirmore <reigo.hirmo@ext.ec.europa.eu>
2024-07-15 22:48:31 +0200

    Monkeypatching PRODCOM correspondance table

--- sessionStorage.js
+++ sessionStorage.js
@@ -1,9 +1,12 @@
 export function prepareAndStoreCorrespondenceData(formattedData) {
+	formattedData.forEach(el=> {
+		console.log(el.text === "PRODCOM 2019-2021"? el.data.pastYear|| "x": el.text);
+	});
 	const storageData = formattedData.map(item => ({
 		thisYear: Number(item.id),
-		pastYear: Number(item.data.pastYear),
-		nextYear: Number(item.data.nextYear),
-		correspUri: item.data.correspUri,
+		pastYear: item.text === "PRODCOM 2021" ? 2019 : Number(item.data.pastYear),
+		nextYear: item.text === "PRODCOM 2019-2020" ? 2021 : Number(item.data.nextYear),
+		correspUri: item.text === "PRODCOM 2019-2020" ? "http://data.europa.eu/qw1/prodcom2019/PRODCOM2019_PRODCOM2021" : item.data.correspUri,
 	}));
 	sessionStorage.setItem("correspondence-table", JSON.stringify(storageData));
 }
