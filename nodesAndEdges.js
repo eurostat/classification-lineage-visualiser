a2e3649be9555330a3facf54dad830b7cc6b0d89
hirmore <reigo.hirmo@ext.ec.europa.eu>
2024-07-23 11:28:16 +0200

    modularisation for both ways

--- nodesAndEdges.js
+++ nodesAndEdges.js
@@ -15,7 +15,16 @@ function createEdgeAndAddToSet(edges, sourceNodeKey, targetNodeKey) {
   edges.add(JSON.stringify(edge));
 }
 
-export function setNodesAndEdges(bindings, conceptId, conceptLabel, iYear, targetYear, processedEdges) {
+export function addEdgeIfNew(edges, processedEdges, nodeKey1, nodeKey2) {
+  const edgeKey = [nodeKey1, nodeKey2].sort().join('-');
+  if (!processedEdges.has(edgeKey)) {
+    createEdgeAndAddToSet(edges, nodeKey1, nodeKey2);
+    processedEdges.add(edgeKey);
+  }
+}
+
+export function setNodesAndEdges( bindings, conceptId, conceptLabel, iYear, targetYear, processedEdges, directFamily) {
+
   if (bindings.length === 0) {
     return { nodes: new Set(), edges: new Set(), targetIds: [] };
   }
@@ -25,13 +34,13 @@ export function setNodesAndEdges(bindings, conceptId, conceptLabel, iYear, targe
   const targetIds = [];
   const sourceNodeKey = `${conceptId}-${iYear}`;
 
-  createNodeAndAddToSet(sourceNodeKey, conceptLabel, iYear, nodes);
+  if (conceptLabel) createNodeAndAddToSet(sourceNodeKey, conceptLabel, iYear, nodes);
 
   bindings.forEach(
 		({
 			ID: { value: targetId },
 			CODE: { value: targetLabel },
-			CLOSE_MATCH_FAMILY: { value: hasChildren },
+			CLOSE_MATCH_FAMILY: { value: hasChildren } = 'false',
 			CLOSE_MATCH_ID: { value: childId } = {},
 			CLOSE_MATCH_CODE: { value: childLabel } = {},
 		}) => {
@@ -50,12 +59,4 @@ export function setNodesAndEdges(bindings, conceptId, conceptLabel, iYear, targe
 	);
 
   return { nodes, edges, targetIds };
-}
-
-function addEdgeIfNew(edges, processedEdges, nodeKey1, nodeKey2) {
-  const edgeKey = [nodeKey1, nodeKey2].sort().join('-');
-  if (!processedEdges.has(edgeKey)) {
-    createEdgeAndAddToSet(edges, nodeKey1, nodeKey2);
-    processedEdges.add(edgeKey);
-  }
 }
